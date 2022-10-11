4fbb64da6e40d8852c2a9e71f01f25ee83a9349c
SKo <sergey.kolygin@gmail.com>
2022-10-11 14:47:25 +0300

    0.1. FileOutputStream. [#252488]

---
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -0,0 +1,19 @@
+package ru.job4j.io;
+
+import java.io.FileOutputStream;
+
+public class ResultFile {
+    public static void main(String[] args) {
+        int[] x = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        try (FileOutputStream out = new FileOutputStream("result.txt")) {
+            for (int q : x) {
+                for (int w = 0; w < 11; w++) {
+                    out.write(((q * w) + " ").getBytes());
+                    out.write(System.lineSeparator().getBytes());
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
