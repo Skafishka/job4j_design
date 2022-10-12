34ff08dba711757c6bf20fb817758266f6553d3c
SKo <sergey.kolygin@gmail.com>
2022-10-12 12:34:56 +0300

    0.3. BufferedReader. [#252489 #350990]

--- src/main/java/ru/job4j/io/LogFilter.java
+++ src/main/java/ru/job4j/io/LogFilter.java
@@ -3,34 +3,21 @@ package ru.job4j.io;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class LogFilter {
-    public static List<String> filter(String file) {
+    public List<String> filter(String file) {
         List<String> list = new ArrayList<>();
         try (BufferedReader in = new BufferedReader(new FileReader(file))) {
-            list = in.lines()
-                    .filter(q -> q.contains(" 404 "))
-                    .collect(Collectors.toList());
+            list = in.lines().filter(q -> q.contains(" 404 ")).toList();
         } catch (IOException e) {
             e.printStackTrace();
         }
         return list;
     }
 
-    public static void save(List<String> log, String file) {
-        try (PrintWriter out = new PrintWriter(
-                new BufferedOutputStream(
-                        new FileOutputStream(file)
-                ))) {
-            out.println(log);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
     public static void main(String[] args) {
-        List<String> log = filter("log.txt");
-        save(log, "404.txt");
+        LogFilter logFilter = new LogFilter();
+        List<String> log = logFilter.filter("log.txt");
+        log.forEach(System.out::println);
     }
 }
