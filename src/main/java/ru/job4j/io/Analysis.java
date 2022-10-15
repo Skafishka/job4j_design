f51ca5f8ef88af13e3120130e9a7272b32404c6c
SKo <sergey.kolygin@gmail.com>
2022-10-15 13:33:44 +0300

    2. Analyzing server availability. [#859 #351590]

--- src/main/java/ru/job4j/io/Analysis.java
+++ src/main/java/ru/job4j/io/Analysis.java
@@ -12,24 +12,19 @@ public class Analysis {
     public void unavailable(String source, String target) {
         try (BufferedReader in = new BufferedReader(new FileReader(source))) {
             try (PrintWriter out = new PrintWriter(new BufferedOutputStream(new FileOutputStream(target)))) {
-                String string = in.readLine();
                 boolean check = false;
                 for (String tmpString = in.readLine(); tmpString != null; tmpString = in.readLine()) {
                     if (!check && tmpString.startsWith("500") || !check && tmpString.startsWith("400")) {
                         rsl = tmpString.substring(tmpString.indexOf(' ') + 1) + ";";
                         out.print(rsl);
-                        System.out.print(rsl);
                         check = true;
                     }
                     if (check && tmpString.startsWith("300") || tmpString.startsWith("200")) {
                         rsl = tmpString.substring(tmpString.indexOf(' ') + 1) + ";" + lineSeparator();
                         out.print(rsl);
-                        System.out.print(rsl);
                         check = false;
                     }
                 }
-            } catch (IOException e) {
-                e.printStackTrace();
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -38,6 +33,7 @@ public class Analysis {
 
     public static void main(String[] args) {
         try (PrintWriter out = new PrintWriter(new FileOutputStream("/data/server.csv"))) {
+            out.println("200 10:56:01");
             out.println("200 10:56:01");
             out.println("500 10:57:01");
             out.println("400 10:58:01");
