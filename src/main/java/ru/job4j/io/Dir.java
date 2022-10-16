7e74478f61f340afbceee6cdd4142eeeaca37cc0
SKo <sergey.kolygin@gmail.com>
2022-10-16 22:12:13 +0300

    4.0. File [#252491 #352286]

--- src/main/java/ru/job4j/io/Dir.java
+++ src/main/java/ru/job4j/io/Dir.java
@@ -5,7 +5,7 @@ import java.io.File;
 public class Dir {
     public static void main(String[] args) {
         File file = new File("C:/projects");
-        if(!file.exists()) {
+        if (!file.exists()) {
             throw new IllegalArgumentException(String.format("Not exist %s", file.getAbsoluteFile()));
         }
         if (!file.isDirectory()) {
