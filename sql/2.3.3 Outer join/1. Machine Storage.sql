5eb9c8be166ae016148801c59b109ad90491ddaa
Skafishka <sergey.kolygin@gmail.com>
2022-11-10 18:55:48 +0200

    0. Machine storage [#1733 #359191]

--- sql/2.3.3 Outer join/1. Machine Storage.sql
+++ sql/2.3.3 Outer join/1. Machine Storage.sql
@@ -22,18 +22,35 @@ create table cars(
 );
 
 insert into car_bodies(name) values ('sedan'), ('hatchback'), ('pickup'), ('universal'), ('jeep'), ('cabriolet');
-insert into car_engines(name) values ('fourstroke diesel'), ('2 liters bensin'), ('ecodrive bensin'), ('twostroke diesel');
-insert into car_transmissions(name) values ('automat'), ('mechanic'), ('semiautomat');
-insert into cars(name, body_id, engine_id, transmission_id) values ('Ford', 1, 1, 1), ('Shelby', 1, 2, 3), 
-('VW', 4, 3, 3), ('Jeep', 5, 1, 1), ('Subaru', 2, 2, 3);
+insert into car_engines(name) values ('fourstroke diesel'), 
+('2 liters bensin'), 
+('ecodrive bensin'), 
+('twostroke diesel');
+insert into car_transmissions(name) values ('automat'), 
+('mechanic'), 
+('semiautomat');
+insert into cars(name, body_id, engine_id, transmission_id) values ('Ford', 1, 1, 1), 
+('Shelby', 1, 2, 3), 
+('VW', 4, 3, 3), 
+('Jeep', 5, 1, 1), 
+('Subaru', 2, 2, 3);
 
 select c.id, c.name car_name, cb.name body_name, ce.name engine_name, ct.name transmissions_name from cars c
 left join car_bodies cb on c.body_id = cb.id
 left join car_engines ce on c.engine_id = ce.id
 left join car_transmissions ct on c.transmission_id = ct.id;
 
-select * from car_bodies cb left join cars c on c.body_id = cb.id where c.body_id is null;
+select * from car_bodies cb 
+left join cars c 
+on c.body_id = cb.id 
+where c.body_id is null;
 
-select * from car_engines ce left join cars c on c.engine_id = ce.id where c.engine_id is null;
+select * from car_engines ce 
+left join cars c 
+on c.engine_id = ce.id 
+where c.engine_id is null;
 
-select * from car_transmissions ct left join cars c on c.transmission_id = ct.id where c.transmission_id is null;
+select * from car_transmissions ct 
+left join cars c 
+on c.transmission_id = ct.id 
+where c.transmission_id is null;
