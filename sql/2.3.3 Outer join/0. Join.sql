24ad5b739d763db23876e426ecf261907f9b0d9a
Skafishka <sergey.kolygin@gmail.com>
2022-11-10 18:51:20 +0200

    0. Join [#6862 #359179]

--- sql/2.3.3 Outer join/0. Join.sql
+++ sql/2.3.3 Outer join/0. Join.sql
@@ -1,20 +1,33 @@
-create table employees(
+create table departments(
 	id serial primary key,
 	name varchar(255)
 );
 
-create table departments(
+create table employees(
 	id serial primary key,
 	name varchar(255),
-	employees_id int references employees(id)
+	department_id int references departments(id)
 );
 
-insert into employees(name) values ('George'), ('Alex'), ('Joe'), ('John'), ('Elmira');
-insert into departments(name, employees_id) values ('Estimation department', 2), 
-('Cost department', 1), 
-('Execution department', 3), 
-('Cost department', 4),
-('Engineering department', null);
+insert into departments(name) values ('Estimation department'),
+('Cost department'),
+('Execution department'),
+('Cost department'),
+('Engineering department');
+insert into employees(name, department_id) values ('George', 1),
+('Alex', 2),
+('Joe', 3),
+('John', 4),
+('Elmira', 1),
+('Elon', null);
+
+select * from departments d left join employees e on d.id = e.department_id;
+select * from departments d right join employees e on d.id = e.department_id;
+select * from departments d full join employees e on d.id = e.department_id;
+select * from departments d left join employees e on d.id = e.department_id where e.id is null;
+
+select * from departments d left join employees e on d.id = e.department_id where e.department_id is not null;
+select * from departments d right join employees e on d.id = e.department_id where e.department_id is not null;
 
 create table teens(
 	name varchar(255),
@@ -22,14 +35,6 @@ create table teens(
 );
 
 insert into teens(name, gender) values ('Henri', 'male'), ('Alex', 'male'), ('Kate', 'female'), ('Angeli', 'female'), ('Kris', 'male');
-
-select * from departments d left join employees e on d.employees_id = e.id;
-select * from departments d right join employees e on d.employees_id = e.id;
-select * from departments d full join employees e on d.employees_id = e.id;
-select * from departments d left join employees e on d.employees_id = e.id where e.id is null;
-
-select * from departments d left join employees e on d.employees_id = e.id;
-select * from departments d right join employees e on d.employees_id = e.id;
 
 select m.name, f.name
 from teens m cross join teens f
