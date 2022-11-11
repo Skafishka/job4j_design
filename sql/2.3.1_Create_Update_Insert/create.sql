6a51ae1f1ba2278ab2b7fb87c21a0217a738a10e
Skafishka <sergey.kolygin@gmail.com>
2022-11-11 12:41:29 +0200

    1. Scheme of rights users and roles. [#1093 #358239]

--- sql/2.3.1_Create_Update_Insert/create.sql
+++ sql/2.3.1_Create_Update_Insert/create.sql
@@ -1,12 +1,12 @@
+create table role (
+	id serial primary key,
+	roles text
+);
+
 create table users (
 	id serial primary key,
 	name text,
 	role_id int references role(id)
-);
-
-create table role (
-	id serial primary key,
-	roles text
 );
 
 create table rules (
@@ -17,27 +17,7 @@ create table rules (
 create table role_rules (
 	id serial primary key,
 	role_id int references role(id),
-	rules_id int referens rules(id)
-);
-
-create table item (
-	id serial primary key,
-	items text,
-	users_id int references users(id),
-	comments_id int references comments(id),
-	attachs_id int references attachs(id),
-	category_id int references category(id),
-	state_id int references state(id)
-);
-
-create table comments (
-	id serial primary key,
-	comment text
-);
-
-create table attachs (
-	id serial primary key,
-	attach text
+	rules_id int references rules(id)
 );
 
 create table category (
@@ -48,4 +28,24 @@ create table category (
 create table state (
 	id serial primary key,
 	state text
+);
+
+create table item (
+	id serial primary key,
+	items text,
+	users_id int references users(id),
+	category_id int references category(id),
+	state_id int references state(id)
+);
+
+create table comments (
+	id serial primary key,
+	comment text,
+	item_id int references item(id)
+);
+
+create table attachs (
+	id serial primary key,
+	attach text,
+	item_id int references item(id)
 );
--- sql/2.3.1_Create_Update_Insert/insert.sql
+++ sql/2.3.1_Create_Update_Insert/insert.sql
@@ -1,8 +1,8 @@
-insert into users(name) values('David');
 insert into role(roles) values ('CEO');
+insert into users(name) values('David');
 insert into rules(rule) values ('admin');
+insert into category(category) values ('urgent');
+insert into state(state) values ('under confirmation');
 insert into item(items) values ('to buy a snack');
 insert into comments(comment) values ('a lot of snacks');
 insert into attachs(attach) values ('photoshoot of snack');
-insert into category(category) values ('urgent');
-insert into state(state) values ('under confirmation');
