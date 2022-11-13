dce5251366e153887fd3bb2e115b23eeee634b78
SKo <sergey.kolygin@gmail.com>
2022-11-13 12:53:25 +0200

    1. Views [#504792 #359964]

--- sql/2.3.4 Database objects/1. Views/2. create view.sql
+++ sql/2.3.4 Database objects/1. Views/2. create view.sql
@@ -1,8 +1,8 @@
-create view show_students_with_2_or_more_books
+create view show_students_with_1_or_more_books
     as select s.name as student, count(a.name), a.name as author from students as s
          right join orders o on s.id = o.student_id
          full join books b on o.book_id = b.id
          left join authors a on b.author_id = a.id
          group by (s.name, a.name) having count(a.name) >= 1;
 
-select * from show_students_with_2_or_more_books;
+select * from show_students_with_1_or_more_books;
