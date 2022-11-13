2a5e756da07570f0cd3d88318057ad429922f780
SKo <sergey.kolygin@gmail.com>
2022-11-13 13:33:04 +0200

    3. Stored procedures and functions [#504804 #360335]

--- sql/2.3.4 Database objects/2. Triggers/3. Stored procedures and functions.sql
+++ sql/2.3.4 Database objects/2. Triggers/3. Stored procedures and functions.sql
@@ -1,14 +1,14 @@
-create or replace procedure delete_data(u_count integer)
+create or replace procedure delete_data_if_less_than_zero(u_count integer, u_id integer)
 language 'plpgsql'
 as $$
     BEGIN
-	if u_count <= 0 THEN
-    	delete from products where id = u_id;
-	end if;
+	    if u_count <= 0 THEN
+    	    delete from products
+    	    where id = u_id;
     END;
 $$;
 
-create or replace function f_delete_data(u_count integer)
+create or replace function f_delete_data_if_less_than_zero(u_count integer, u_id integer)
 returns void
 language 'plpgsql'
 as
